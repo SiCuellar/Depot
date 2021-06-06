34a0ddf6cb62514a3eb006c2a428c21bf7d095ba
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2021-06-05 19:38:39 -0700

    adds title validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
     validates :title, :description, :image_url, :presence: true
     validates :price, numericality: { greater_thanor_equal_to: 0.01 }
-end
+    validates :title, uniqueness: true
